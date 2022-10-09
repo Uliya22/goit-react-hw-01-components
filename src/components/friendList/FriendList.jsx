7592bc5ef4bf5273d3989cf70807361fd8ed4f7d
Uliya22 <uliya.d@ukr.net>
2022-10-09 22:38:39 +0300

    Завершення стилізації

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from './FriendListItem';
-
+import css from './FriendList.module.css';
 export const FriendList = ({ friends }) => {
   return (
-    <ul className="friend-list">
+    <ul className={css.friendList}>
       {friends.map(friend => (
         <FriendListItem
           key={friend.id}
